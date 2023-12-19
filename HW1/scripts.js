2ea8ee1c5af9969fa997065347260e9d803090ff
ArtKruzhkov <112978976+ArtKruzhkov@users.noreply.github.com>
2023-12-19 19:52:13 +0300

    HW01

---
+++ HW1/scripts.js
@@ -0,0 +1,11 @@
+//task1
+
+let tempCelcium = 15;
+let tempFahrenheit = (9 / 5) * tempCelcium + 32;
+console.log(`Temperature Celcium: ${tempCelcium}\nTemperatura Fehrenheit: ${tempFahrenheit}`);
+
+//task2
+
+let firstName = 'Artem';
+let admin = firstName;
+console.log(admin);
